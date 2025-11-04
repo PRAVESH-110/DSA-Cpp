2c3882c9648a211eb73b4699d64a0486726f3fb1
PRAVESH DHAKAL <pravdhakal110@gmail.com>
2025-11-04 16:58:16 +0530

    overloading

---
+++ 42functoverload.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+
+//2/more funct can have the same name but must differ in the parameter
+// functions can have same name but also must have diff return type (eg: )
+class A {
+    public:
+ 
+    //this will work as they have different return parameter but 
+    void sayHello(){
+        cout<<"Hello pd"<<endl;
+    }
+
+    int sayHello(string s){
+        cout<<"hello"<<s<<endl;
+    }
+
+    //this couldnt be used
+    // void sayHello(){
+    //     cout<<"Hello pd"<<endl;
+    // }
+
+    // int sayHello(){
+    //     cout<<"hello"<<s<<endl;
+    // }
+};
+int main(){
+    A obj;
+    obj.sayHello();
+
+    return 0;
+}
