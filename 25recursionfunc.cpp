6778db3ba56bde279a08e2d1dab568ba903fa33c
PRAVESH DHAKAL <pravdhakal110@gmail.com>
2025-12-18 16:39:16 +0530

    recursion

--- 25recursionfunc.cpp
+++ 25recursionfunc.cpp
@@ -6,8 +6,8 @@
 using namespace std;
 
 int fibonacci(int n){
-    if(n==0) return 0;
-    if(n==1) return 1;
+    if(n==1) return 0;
+    if(n==2) return 1;
 
     int ans=fibonacci(n-1)+fibonacci(n-2);
     return ans;
