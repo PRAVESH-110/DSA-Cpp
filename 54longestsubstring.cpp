51e35c97f65565e9f5c8f81e773aa18920b1f814
PRAVESH DHAKAL <pravdhakal110@gmail.com>
2026-03-12 11:05:22 +0530

    doit

--- 54longestsubstring.cpp
+++ 54longestsubstring.cpp
@@ -1,20 +1,27 @@
 //find the longest substring without repeating character, and return only length of substring
 #include<iostream>
 #include<string>
+#include<vector>
 using namespace std;
 
 int subString(string s){
-    int l=0;
-    int r=l+1;
-    int hash[256]={-1};
-    for(int i=0;i<s.length();i++){
-        if(hash[s[i]]==-1){
-            r++;
-        }
-        l=i+1;
-        r=l+1;
-    }
+    vector<int> lastIndex(256, -1);
 
+        int l = 0;
+        int maxLen = 0;
+
+        for(int r = 0; r < s.size(); r++) {
+
+            if(lastIndex[s[r]] != -1) {
+                l = max(l, lastIndex[s[r]] + 1);
+            }
+
+            lastIndex[s[r]] = r;
+
+            maxLen = max(maxLen, r - l + 1);
+        }
+
+        return maxLen;
 }
 int main(){
     string s="cadbzabcd";
